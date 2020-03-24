96470d40c6f886f181f08129e806930fc1f979bd
SergeevSergey99 <sergeev_sergey99@mail.ru>
2020-03-25 01:18:55 +0300

    Initial commit

---
+++ Source/WayOfMind/WayOfMindGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WayOfMindGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WAYOFMIND_API AWayOfMindGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
